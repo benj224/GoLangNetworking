d384ecbd9aca7fce3172ff5b61fc028d53024ade
benj224 <52797746+benj224@users.noreply.github.com>
2021-10-25 17:29:13 +0100

    merged

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"net"
-	"sync"
 
 	"github.com/perlin-network/noise"
 )
@@ -13,74 +12,6 @@ func main() {
 
 	reciever()
 	//sender()
-	// // Let there be nodes Alice and Bob.
-
-	// alice, err := noise.NewNode()
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// bob, err := noise.NewNode()
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// // Gracefully release resources for Alice and Bob at the end of the example.
-
-	// defer alice.Close()
-	// defer bob.Close()
-
-	// var wg sync.WaitGroup
-
-	// // When Alice gets a message from Bob, print it out.
-
-	// alice.Handle(func(ctx noise.HandlerContext) error {
-	// 	fmt.Printf("Got a message from Bob: '%s'\n", string(ctx.Data()))
-	// 	wg.Done()
-	// 	return nil
-	// })
-
-	// // When Bob gets a message from Alice, print it out.
-
-	// bob.Handle(func(ctx noise.HandlerContext) error {
-	// 	fmt.Printf("Got a message from Alice: '%s'\n", string(ctx.Data()))
-	// 	wg.Done()
-	// 	return nil
-	// })
-
-	// // Have Alice and Bob start listening for new peers.
-
-	// if err := alice.Listen(); err != nil {
-	// 	panic(err)
-	// }
-
-	// if err := bob.Listen(); err != nil {
-	// 	panic(err)
-	// }
-
-	// // Have Alice send Bob 'Hi Bob!'
-
-	// if err := alice.Send(context.TODO(), bob.Addr(), []byte("Hi Bob!")); err != nil {
-	// 	panic(err)
-	// }
-
-	// // Wait until Bob receives the message from Alice.
-
-	// wg.Add(1)
-	// wg.Wait()
-
-	// // Have Bob send Alice 'Hi Alice!'
-
-	// if err := bob.Send(context.TODO(), alice.Addr(), []byte("Hi Alice!")); err != nil {
-	// 	panic(err)
-	// }
-
-	// // Wait until Alice receives the message from Bob.
-
-	// wg.Add(1)
-	// wg.Wait()
-
-	// fmt.Println(alice.Addr())
 
 }
 
@@ -107,29 +38,14 @@ func reciever() {
 }
 
 func sender() {
-	sender, err := noise.NewNode()
+	sender, err := noise.NewNode(noise.WithNodeBindHost(net.ParseIP("192.168.1.126")), noise.WithNodeBindPort(49972))
 	if err != nil {
 		panic(err)
 	}
 
 	defer sender.Close()
 
-	var wg sync.WaitGroup
-
-	wg.Add(1)
-	wg.Wait()
-
-	sender.Handle(func(ctx noise.HandlerContext) error {
-		fmt.Printf("Got a message: '%s'\n", string(ctx.Data()))
-		wg.Done()
-		return nil
-	})
-
-	if err := sender.Listen(); err != nil {
-		panic(err)
-	}
-
-	if err := sender.Send(context.TODO(), "192.168.1.114:57582", []byte("Hi There")); err != nil {
+	if err := sender.Send(context.TODO(), "192.168.1.114:49972", []byte("Hi There")); err != nil {
 		panic(err)
 	}
 }
