55d87ad42f7638f19fc2c09fe752c8b0c487d332
benj224 <52797746+benj224@users.noreply.github.com>
2021-10-30 07:50:49 -0700

    ,d\bzx.k

--- go.mod
+++ go.mod
@@ -3,13 +3,13 @@ module github.com/benj224/GoLangNetworking
 go 1.16
 
 require (
-	github.com/davecgh/go-spew v1.1.1
-	github.com/ipfs/go-log v1.0.5
-	github.com/libp2p/go-libp2p v0.15.1
-	github.com/libp2p/go-libp2p-core v0.11.0
-	github.com/libp2p/go-libp2p-host v0.1.0
-	github.com/libp2p/go-libp2p-net v0.1.0
-	github.com/libp2p/go-libp2p-peerstore v0.2.8
-	github.com/multiformats/go-multiaddr v0.4.0
 	github.com/perlin-network/noise v1.1.3
+	github.com/pkg/errors v0.9.1 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	go.uber.org/zap v1.19.0 // indirect
+	golang.org/x/crypto v0.0.0-20210813211128-0a44fdfbc16e // indirect
+	golang.org/x/lint v0.0.0-20200302205851-738671d3881b // indirect
+	golang.org/x/tools v0.1.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -13,58 +13,66 @@ import (
 func main() {
 	// Let there be Alice, Bob, and Charlie.
 
-	alice, err := noise.NewNode()
-	if err != nil {
-		panic(err)
-	}
+	// alice, err := noise.NewNode()
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	bob, err := noise.NewNode()
 	if err != nil {
 		panic(err)
 	}
 
-	charlie, err := noise.NewNode()
-	if err != nil {
-		panic(err)
-	}
+	// charlie, err := noise.NewNode()
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	// Alice, Bob, and Charlie are following an overlay network protocol called Kademlia to discover, interact, and
 	// manage each others peer connections.
 
-	ka, kb, kc := kademlia.New(), kademlia.New(), kademlia.New()
+	//ka, kb, kc := kademlia.New(), kademlia.New(), kademlia.New()
+	kb := kademlia.New()
 
-	alice.Bind(ka.Protocol())
+	//alice.Bind(ka.Protocol())
 	bob.Bind(kb.Protocol())
-	charlie.Bind(kc.Protocol())
+	//charlie.Bind(kc.Protocol())
 
-	if err := alice.Listen(); err != nil {
-		panic(err)
-	}
+	// if err := alice.Listen(); err != nil {
+	// 	panic(err)
+	// }
 
 	if err := bob.Listen(); err != nil {
 		panic(err)
 	}
 
-	if err := charlie.Listen(); err != nil {
-		panic(err)
-	}
+	// if err := charlie.Listen(); err != nil {
+	// 	panic(err)
+	// }
 
 	// Have Bob and Charlie learn about Alice. Bob and Charlie do not yet know of each other.
+	// nd, err := bob.Ping(context.TODO(), "192.168.1.117:49972")
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	if _, err := bob.Ping(context.TODO(), alice.Addr()); err != nil {
+	err = bob.Send(context.TODO(), "192.168.1.117:49972", []byte("test"))
+	if err != nil {
 		panic(err)
 	}
 
-	if _, err := charlie.Ping(context.TODO(), bob.Addr()); err != nil {
-		panic(err)
-	}
+	// if _, err := charlie.Ping(context.TODO(), bob.Addr()); err != nil {
+	// 	panic(err)
+	// }
 
 	// Using Kademlia, Bob and Charlie will learn of each other. Alice, Bob, and Charlie should
 	// learn about each other once they run (*kademlia.Protocol).Discover().
 
-	fmt.Printf("Alice discovered %d peer(s).\n", len(ka.Discover()))
+	//fmt.Printf("Alice discovered %d peer(s).\n", len(ka.Discover()))
+	// fmt.Printf(nd.ID().Address)
+	// kb.Ack(nd.ID())
 	fmt.Printf("Bob discovered %d peer(s).\n", len(kb.Discover()))
-	fmt.Printf("Charlie discovered %d peer(s).\n", len(kc.Discover()))
+	//fmt.Printf("Charlie discovered %d peer(s).\n", len(kc.Discover()))
 
 	// Output:
 	// Alice discovered 2 peer(s).
